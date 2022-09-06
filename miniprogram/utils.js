0e2db7f39777b26cfdb6c24bcb4b01eab13ec82b
lyj-mac <lyj-mac.com>
2022-09-06 18:57:01 +0800

    upate some page

--- miniprogram/utils.js
+++ miniprogram/utils.js
@@ -12,30 +12,42 @@ var fileName='infos.json'
 
 
 function loadData(){
-  const fs = wx.getFileSystemManager()
+  
   const ab = new ArrayBuffer(1024)
   const file = `${wx.env.USER_DATA_PATH}/`+fileName
   console.log(`open file: ${file}`)
   var ls=[]
   try{
+    const fs = wx.getFileSystemManager()
+    fs.accessSync(file)
     const fd = fs.openSync({
       filePath: file,
-      flag: 'a+'
+      flag: 'r'
     })
     const res = fs.readSync({
       fd: fd,
       arrayBuffer: ab,
-      length: 10
+      length: 1024 
     })
     console.log(res)
-
-    return JSON.parse(ab)
+    // 关闭文件
+    fs.closeSync({fd: fd})
+    // var jsongString = String.fromCharCode.apply(null,new Uint16Array(ab))
+    var dec= new TextDecoder("utf-8")
+    var jsonString=dec.decode(ab)
+    console.log(jsonString)
+    // var obj= eval('('+jsonString+')')
+    
+    var jst= '[{"name":"万科城","imageUrl":"3434"}]'
+    // var jst = "'"+jsonString+"'"
+    return JSON.parse(jst)
 
   }catch{
+    
     console.error("no json file")
-    return ls
+    
   }
-  
+  return ls 
 
 }
 function loadDataDemo(){
@@ -56,19 +68,28 @@ function loadDataDemo(){
 function saveData(obj){
   // 同步接口
   try {
-    const res = fs.writeFileSync(
-      `${wx.env.USER_DATA_PATH}/`+fileName,
+    const fs = wx.getFileSystemManager()
+    const file = `${wx.env.USER_DATA_PATH}/`+fileName
+    // const fd = fs.openSync({
+    //   filePath: file,
+    //   flag: 'a+'
+    // })    
+    fs.writeFileSync(
+      file,
       obj,
       'utf8'
     )
-    console.log(res)
+    // console.log(res)
+    // 关闭文件
+    // fs.closeSync({fd: fd})
   } catch(e) {
     console.error(e)
   }
 }
 function add(obj){
-  var list = loadData()
-  console.log(`get datalist:${list}`)
-  list.append(obj)
-  saveData(list)
+  var ls = loadData()
+  // var ls=[]
+  console.log(`get datalist:${ls}`)
+  ls.push(obj)
+  saveData(JSON.stringify(ls))
 }
